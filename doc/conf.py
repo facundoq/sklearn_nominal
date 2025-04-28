f738998f1f5477e206b26741d0eb07771b71cfc8
facundoq <facundoq@gmail.com>
2025-04-28 13:18:41 -0300

    first version

--- doc/conf.py
+++ doc/conf.py
@@ -58,7 +58,7 @@ html_sidebars = {
 
 html_theme_options = {
     "external_links": [],
-    "github_url": "https://github.com/facundoq/scikit-learn-models",
+    "github_url": "https://github.com/facundoq/sklearnmodels",
     # "twitter_url": "https://twitter.com/pandas_dev",
     "use_edit_page_button": True,
     "show_toc_level": 1,
@@ -67,7 +67,7 @@ html_theme_options = {
 
 html_context = {
     "github_user": "facundoq",
-    "github_repo": "scikit-learn-models",
+    "github_repo": "sklearnmodels",
     "github_version": "master",
     "doc_path": "doc",
 }
